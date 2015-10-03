7c516b3f4014affd4a87cd54c72c213c89905d84
katernst <katernst@umich.edu>
2015-10-03 10:13:35 -0400

    Updated everything

--- src/app.js
+++ src/app.js
@@ -4,198 +4,16 @@
  * This is where you write your app.
  */
 
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
 var Vector2 = require('vector2');
-var Vector3 = require('vector3');
-var Vector4 = require('vector4');
-var Vector5 = require('vector5');
-var total_attended = 0;
-var total_skipped = 0;
-var grades_arr = [];
-
+//var VectorLebronSad = require('vectorLebronS');
+var set = 0;
+var streak = 10;
+var total = 40;
 
 
 var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'NOTHING IS GIVEN, EVERYTHING IS EARNED',
-  body: 'Press any button.'
-});
-
-main.show();
-
-main.on('click', 'select', function(e) {
-        var menu = new UI.Menu({
-        sections: [{
-          items: [{
-          title: 'Attendance',
-          icon: 'images/happy_lebron-color.png',
-          subtitle: 'Did you go to school?'
-          }, {
-          title: 'Grades',
-          subtitle: 'Keep up in school'
-          }, {
-          title: 'Community Service',
-          subtitle: 'Log your hours'
-          }]
-        }]
-        });
-        menu.on('select', function(e) {
-        console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-        console.log('The item is titled "' + e.item.title + '"');
-        var streak = 0;
-    
-        if(e.itemIndex === 0) {
-        var attendanceCard = new UI.Card();
-        attendanceCard.show();
-        attendanceCard.title('Where are you?');
-        attendanceCard.subtitle('Are you in class?');
-      
-        attendanceCard.on('click', 'up', function(e) {
-        var image = new UI.Image({
-          position: new Vector3(0, 0),
-          size: new Vector3(144, 168),
-          image: 'happylebronreal.png'
-        });
-        attendanceCard.add(image);
-        image.show();
-        streak += 1;
-        attendanceCard.body("STREAK: " + streak);
-        attendanceCard.show();
-        total_attended += 1;
-
-        });
-        attendanceCard.on('click', 'down', function(e) {
-        var image2 = new UI.Image({
-          position: new Vector4(0, 0),
-          size: new Vector4(144, 168),
-          image2: 'sadlebronreal.png'
-        });
-        attendanceCard.add(image2);
-        image2.show();
-        streak = 0;
-        attendanceCard.body("STREAK: " + streak);
-        attendanceCard.show();
-        total_skipped += 1;
-      });
-        }
-                        
-    if(e.itemIndex === 1) {
-      var mathGrade;
-      var engGrade;
-      var histGrade;
-      var langGrade;
-      var sciGrade;
-      var electGrade;
-
-      var menuForGrades = new UI.Menu({
-      //SET THESE^^
-       sections: [{
-       items: [{
-         title: 'Math',
-         subtitle: mathGrade
-        }, {
-         title: 'English',
-         subtitle: engGrade
-        }, {
-          title: 'Science',
-          subtitle: sciGrade
-        }, {
-          title: 'Language',
-          subtitle: langGrade}, 
-        {
-          title: 'History',
-          subtitle: histGrade }, 
-        {
-          title: 'Elective',
-          subtitle: electGrade
-        }]
-     }]
-    });
-
-        menuForGrades.on('select', function(e) {
-        var cardForMenu = new UI.Card({
-          position: new Vector5(0, 0),
-          size: new Vector5(144, 168),
-        });
-        var letterGrades = ["B+", "B", "B-", "C+", "C","C-","D+", "D", "D-", "A+", "A", "A-"];
-        cardForMenu.title = letterGrades[0];
-        var currentGrade = 0;
-        cardForMenu.show();
-
-        //WAY TO SAVE TO DATA??
-
-         cardForMenu.on('click', 'down', function(e) {
-             currentGrade = (currentGrade + 1) % 12;
-             cardForMenu.title = letterGrades[currentGrade]; 
-             grades_arr.push(letterGrades[currentGrade]);
-             cardForMenu.show();
-      });
-
-
-      //var gradesCard = new UI.Card();
-      //gradesCard.subtitle('button' + e.itemIndex + 'ok');
-      //gradesCard.show();
-    
-        )};
-    }
-                         
-                         
-    if (e.itemIndex === 2) {
-      var communityCard = new UI.Card();
-      communityCard.subtitle('button' + e.itemIndex + 'ok');
-      communityCard.show();
-    }
-    
-        });
-  menu.show();
-});
-
-
-
-
-main.on('click', 'up', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
-
-
-
-/*
-var UI = require('ui');
-var Vector2 = require('vector2');
-var Vector3 = require('vector3');
-
-
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'NOTHING IS GIVEN, EVERYTHING IS EARNED',
-  body: 'Press any button.'
+  subtitle: 'NOTHING IS GIVEN, EVERYTHING IS EARNED'
 });
 
 main.show();
@@ -217,63 +35,147 @@ main.on('click', 'select', function(e) {
     }]
   });
   menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-    
-    if(e.itemIndex === 0) {
-      var attendanceCard = new UI.Card();
-      attendanceCard.title('Where are you?');
-      attendanceCard.subtitle('Are you in class?');
-      
-      attendanceCard.on('click', 'up', function(e) {
-        var image = new UI.Image({
-          position: new Vector3(0, 0),
-          size: new Vector3(144, 168),
-          image: 'images/your_image.png'
+    if(e.item.title === 'Attendance') {
+      var attendanceCard = new UI.Window({
+        fullscreen: true,
+        //backgroundcolor: 'white'
+       });
+      var streak_text = new UI.Text({
+        position: new Vector2(0, 50),
+        size: new Vector2(144, 30),
+        font: 'gothic-24-bold',
+        text: 'Streak: ' + streak.toString(),
+        textAlign: 'center'
+      });
+      var total_text = new UI.Text({
+        position: new Vector2(0, 70),
+        size: new Vector2(144, 30),
+        font: 'gothic-24-bold',
+        text: 'Total Days: ' + total.toString(),
+        textAlign: 'center'
+      });
+      var in_school = new UI.Text({
+        position: new Vector2(35, 0),
+        size: new Vector2(144, 30),
+        font: 'gothic-24-bold',
+        text: 'In School?',
+        textAlign: 'center'
+      });
+      var not_in_school = new UI.Text({
+        position: new Vector2(33, 130),
+        size: new Vector2(144, 30),
+        font: 'gothic-24-bold',
+        text: 'No School?',
+        textAlign: 'center'
+      });
+      var happy_lebron = new UI.Image({
+        //added stuff here
+     
+        image: 'HAPPY_LEBRON'
+      });
+      var sad_lebron = new UI.Image({
+        //position: new VectorLebronSad(0,0),
+        //size: new VectorLebronSad(144,168),
+        image: 'SAD_LEBRON'
+      });
+      attendanceCard.add(in_school);
+      attendanceCard.add(not_in_school);
+      attendanceCard.add(total_text);
+      attendanceCard.add(streak_text);
+      //get rid of this if it doesn't work
+      attendanceCard.show();
+      attendanceCard.on('click', 'up', function(){
+        if (set === 0) {
+          streak = streak + 1;
+          total = total + 1;
+          set = 1;
+        }
+        
+        attendanceCard.add(happy_lebron);
+        //attendanceCard.show();
+        streak_text.text('Total Days: ' + total.toString());
+        streak_text.text('Streak Days: ' + streak.toString());
+        attendanceCard.add(streak_text);
+        attendanceCard.add(total_text);
+      });
+      attendanceCard.on('click', 'down', function(){
+        if (set === 0) {
+          streak = 0;
+          set = 1;
+        }
+        attendanceCard.add(sad_lebron);
+        streak_text.text('Streak Days: ' + streak.toString());
+        attendanceCard.add(streak_text);
+      });
+
+    }
+    if(e.item.title === 'Grades') {
+      var gradesmenu = new UI.Menu({
+        sections: [{
+          items: [{
+            title: 'Math',
+          }, 
+          {
+          title: 'English',
+          },
+          {
+          title: 'Language',
+          },
+                  {
+          title: 'History',
+          }, 
+                  {
+          title: 'Elective',
+          }, {
+          title: 'Science',
+          }]
+        }]
+      });
+      gradesmenu.on('select', function(e) {
+        var lettergrademenu = new UI.Menu({
+          sections: [{
+            items: [{
+              title: 'A',
+            }, {
+              title: 'B',
+            }, {
+              title: 'C',
+            }, {
+              title: 'D',
+            }, {
+              title: 'F',        
+            }]
+          }]
         });
-        attendanceCard.add(image);
-        attendanceCard.show();
-    });
-    }
-                        
-    if(e.itemIndex === 1) {
-      var gradesCard = new UI.Card();
-      gradesCard.subtitle('button' + e.itemIndex + 'ok');
-      gradesCard.show();
-    }
-    if(e.itemIndex === 2) {
-      var communityCard = new UI.Card();
-      communityCard.subtitle('button' + e.itemIndex + 'ok');
-      communityCard.show();
-    }
-    
+        
+       // lettergrademenu.on('select', function(e) {
+        // lettergrademenu.item(e.sectionIndex, e.itemIndex {title:e.item.title, subtitle: 'Current Course Grade'}); 
+        //});
+        
+        lettergrademenu.show();
+      });
+      
+      gradesmenu.show();
+
+    }//end of if grades statement
+    if(e.item.title === 'Community Service') {
+      var serviceWindow = new UI.Window({
+        fullscreen: true,
+        backgroundcolor: 'white'
+       });
+      var serviceTotal = new UI.Text({
+        position: new Vector2(0, 50),
+        size: new Vector2(144, 30),
+        font: 'gothic-24-bold',
+        text: 'Streak: ' + streak.toString(),
+        textAlign: 'center'
+      });
+      serviceWindow.show();
+      serviceTotal.text('Total' + serviceTotal.to_string());
+      serviceWindow.add(serviceTotal);
+    } //end of if service statement
   });
   menu.show();
 });
 
-
-
-
-main.on('click', 'up', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
-*/
+  
